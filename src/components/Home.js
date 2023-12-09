baf74e24ee0c60dea104b586364fab74b073192b
dinshramGovindaraj <gdr19102002@gmail.com>
2023-12-09 13:33:26 +0530

    files added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Avathar from "../images/avathar-removebg-preview.png";
+import { TypeAnimation } from "react-type-animation";
+
 const Home = () => {
   return (
     <>
@@ -9,11 +11,24 @@ const Home = () => {
             <img className="avathar-img" src={Avathar} alt="my image" />
           </div>
           <div className="col-lg-6 col-md-6 col-sm-12">
-            <h1 className="home-head">
+            {/* <h1 className="home-head">
               Hi 👋,
               <br /> My name is <br />{" "}
               <span className="home-span">Dinesh Ram</span> <br /> I build
               things for web
+            </h1>
+            <button className="btn btn-secondary cv-btn">Download CV</button> */}
+            <h1 className="home-head">
+              <TypeAnimation
+                sequence={[
+                  `Hi 👋,\n My name is \n Dinesh Ram \n I build things for web`,
+                  1000,
+                  "",
+                ]}
+                speed={50}
+                style={{ whiteSpace: "pre-line" }}
+                repeat={Infinity}
+              />
             </h1>
             <button className="btn btn-secondary cv-btn">Download CV</button>
           </div>
