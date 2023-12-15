b6743dd4a6051318757181a8b640d4ec6ca1e9ac
dinshramGovindaraj <gdr19102002@gmail.com>
2023-12-15 18:12:16 +0530

    changes added

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,16 +9,9 @@ const Home = () => {
       <div className="container">
         <div className="home-flex row">
           <div className="col-lg-6 col-md-6 col-sm-12">
-            <img className="avathar-img" src={Avathar} alt="my image" />
+            <img className="avathar-img" src={Avathar} alt="avathar" />
           </div>
           <div className="col-lg-6 col-md-6 col-sm-12">
-            {/* <h1 className="home-head">
-              Hi 👋,
-              <br /> My name is <br />{" "}
-              <span className="home-span">Dinesh Ram</span> <br /> I build
-              things for web
-            </h1>
-            <button className="btn btn-secondary cv-btn">Download CV</button> */}
             <h1 className="home-head">
               <TypeAnimation
                 sequence={[
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,86 +1,3 @@
-// import React from "react";
-// import "../styles/Navbar.css";
-// const Navbar = () => {
-//   const [showNavbar, setShowNavbar] = React.useState(false);
-
-//   const handleShowNavbar = () => {
-//     setShowNavbar(!showNavbar);
-//   };
-
-//   return (
-//     <nav className="navbar">
-//       <div className="nav-container container">
-//         <div className="logo">Portfolio</div>
-//         <div className="menu-icon" onClick={handleShowNavbar}>
-//           {/* <Hamburger /> */}
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             width="52"
-//             height="24"
-//             viewBox="0 0 52 24"
-//           >
-//             <g id="Group_9" data-name="Group 9" transform="translate(-294 -47)">
-//               <rect
-//                 id="Rectangle_3"
-//                 data-name="Rectangle 3"
-//                 width="42"
-//                 height="4"
-//                 rx="2"
-//                 transform="translate(304 47)"
-//                 fill="#574c4c"
-//               ></rect>
-//               <rect
-//                 id="Rectangle_5"
-//                 data-name="Rectangle 5"
-//                 width="42"
-//                 height="4"
-//                 rx="2"
-//                 transform="translate(304 67)"
-//                 fill="#574c4c"
-//               ></rect>
-//               <rect
-//                 id="Rectangle_4"
-//                 data-name="Rectangle 4"
-//                 width="52"
-//                 height="4"
-//                 rx="2"
-//                 transform="translate(294 57)"
-//                 fill="#574c4c"
-//               ></rect>
-//             </g>
-//           </svg>
-//         </div>
-//         <div className={`nav-elements  ${showNavbar && "active"}`}>
-//           <ul>
-//             <li>
-//               {/* <NavLink to="/">Home</NavLink> */}
-//               <a>Home</a>
-//             </li>
-//             <li>
-//               {/* <NavLink to="/about">About</NavLink> */}
-//               <a>About</a>
-//             </li>
-//             <li>
-//               {/* <NavLink to="/tech-stack">Tech Stack</NavLink> */}
-//               <a>Tech Stack</a>
-//             </li>
-//             <li>
-//               {/* <NavLink to="/project">Project</NavLink> */}
-//               <a>Project</a>
-//             </li>
-//             <li>
-//               {/* <NavLink to="/contact">Contact</NavLink> */}
-//               <a>Contact</a>
-//             </li>
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
 import "../styles/Navbar.css";
 
@@ -92,48 +9,64 @@ const Navbar = () => {
           className="navbar navbar-expand-lg fixed-top"
           style={{ backgroundColor: "black" }}
         >
-          <a className="navbar-brand" href="#" style={{ color: "white" }}>
-            Dinesh Ram
+          <a className="navbar-brand" style={{ color: "white" }}>
+            <b>Dinesh Ram</b>
           </a>
           <button
             className="navbar-toggler"
             type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarText"
-            aria-controls="navbarText"
-            aria-expanded="true"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar"
             aria-label="Toggle navigation"
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="navbar-collapse collapse " id="navbarText">
-            <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link nav-text" href="#carousel">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-text" href="#about">
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-text" href="#contact">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-text" href="#review">
-                  Review
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link nav-text" href="#login">
-                  Login
-                </a>
-              </li>
-            </ul>
+          <div
+            className="offcanvas offcanvas-end"
+            tabindex="-1"
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            style={{ width: "75%" }}
+          >
+            <div className="offcanvas-header">
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="offcanvas"
+                aria-label="Close"
+                style={{ backgroundColor: "white" }}
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link nav-text" href="#carousel">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link nav-text" href="#about">
+                    About Us
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link nav-text" href="#contact">
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link nav-text" href="#review">
+                    Review
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link nav-text" href="#login">
+                    Login
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </nav>
       </div>
